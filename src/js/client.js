d70a23a5fdaa7171f7ada8a1ad13f0db9b62f0ba
Malte Spielberger <m@lte.cc>
2017-11-26 12:38:51 +0100

    remove unlinked files

--- src/js/client.js
+++ src/js/client.js
@@ -8,9 +8,7 @@ import store from "./store"
 import Layout from "./pages/Layout";
 import LayoutSidebar from "./pages/LayoutSidebar";
 
-import Archives from "./pages/Archives";
 import Featured from "./pages/Featured";
-import Settings from "./pages/Settings";
 import Contact from "./pages/Contact";
 import Shop from "./pages/Shop";
 
@@ -30,8 +28,6 @@ ReactDOM.render(
         <Route path="recipes" name="recipes" component={Recipes}></Route>
         <Route path="recipe/:recipe" name="recipedetail" component={RecipeDetail}></Route>
         <Route path="contact" name="contact" component={Contact}></Route>
-        <Route path="archives(/:product)" name="archives" component={Archives}></Route>
-        <Route path="settings" name="settings" component={Settings}></Route>
       </Route>
     </Router>
   </Provider>,
